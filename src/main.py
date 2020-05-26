20d6673510ad2146b1b1366049018f331ecd4dbe
Lalo Rivero <eduardo.rivero@cobachi.edu.mx>
2020-05-25 22:26:09 -0600

    challenge complete

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
 # Resolve the problem!!
+import re
+
+pattern = re.compile(r'[a-z]+')
 
 
 def run():
-    # Start coding here
+    f = open('encoded.txt', 'r', encoding='utf-8')
+
+    for line in f:
+        result = re.findall(pattern, line)
+        print('Searching...')
+        if result:
+            print(f'Palabra uno: {result[0]}')
+            name = ''.join(result[1:10])
+            lastname = ''.join(result[10:16])
+            lastname2 = ''.join(result[16:])
+            print(f'Palabra dos: {name} {lastname} {lastname2}')
+        else:
+            print('No se encontraron Datos')
+    f.close()
 
 
 if __name__ == '__main__':
